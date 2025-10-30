3cd1397ec968b2c1abb3f7d772ce0a0816f520f4
Mikhail Solovyov <solovev02@yandex.ru>
2025-10-30 19:10:21 +0300

    added Allure

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -1,7 +1,9 @@
 package base;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import data.TestData;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import pages.ProductsPage;
@@ -30,7 +32,7 @@ public class TestBase {
         Configuration.baseUrl = "https://automationexercise.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-
+        SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
     @BeforeEach
