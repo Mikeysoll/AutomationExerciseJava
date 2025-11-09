a7a5ddaf598508f945a45427f6c8822b16c6a4e1
Mikhail Solovyov <solovev02@yandex.ru>
2025-11-09 19:25:30 +0300

    rename tag

--- src/test/java/tests/ProductSearchTest.java
+++ src/test/java/tests/ProductSearchTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-@Tag("first")
+@Tag("third")
 public class ProductSearchTest extends TestBase {
 
     static Stream<String> productNames() {
