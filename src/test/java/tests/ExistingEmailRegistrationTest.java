028902179829ab0fae7dfd3871f0b6620f86057c
Mikhail Solovyov <solovev02@yandex.ru>
2025-11-09 14:37:12 +0300

    added tags

--- src/test/java/tests/ExistingEmailRegistrationTest.java
+++ src/test/java/tests/ExistingEmailRegistrationTest.java
@@ -2,10 +2,12 @@ package tests;
 
 import base.TestBase;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+@Tag("test")
 public class ExistingEmailRegistrationTest extends TestBase {
 
     private static final String EMAIL_EXISTS_ERROR
