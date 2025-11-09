a3eb14c80270bfccf46bfe4bd9f18c163a01b994
Mikhail Solovyov <solovev02@yandex.ru>
2025-11-09 15:52:34 +0300

    changed build.gradle

--- src/test/java/tests/ProductSearchTest.java
+++ src/test/java/tests/ProductSearchTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-@Tag("Search")
+@Tag("search")
 public class ProductSearchTest extends TestBase {
 
     static Stream<String> productNames() {
