0a1f7be066385570556b7b6baa0ebb6f2ce2c827
Mikhail Solovyov <solovev02@yandex.ru>
2025-11-15 18:31:51 +0300

    added tags

--- src/test/java/tests/ProductSearchTest.java
+++ src/test/java/tests/ProductSearchTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
+@Tag("first")
 public class ProductSearchTest extends TestBase {
 
     static Stream<String> productNames() {
