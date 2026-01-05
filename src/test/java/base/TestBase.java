7db0acb49948a82635fb94c660f9efe48235d77d
Mikhail Solovyov <solovev02@yandex.ru>
2026-01-05 18:34:16 +0300

    minor

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -6,9 +6,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import pages.HomePage;
-import pages.ProductsPage;
-import pages.SignupLoginPage;
+import pages.*;
 import pages.components.Header;
 
 import java.time.Duration;
@@ -17,40 +15,43 @@ import java.util.Map;
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Configuration.*;
-
+import com.codeborne.selenide.Configuration;
 
 public class TestBase {
     protected ProductsPage productsPage = new ProductsPage();
     protected SignupLoginPage loginPage = new SignupLoginPage();
     protected HomePage homePage = new HomePage();
+    protected AccountInfoPage accountInfoPage = new AccountInfoPage();
+    protected ContactPage contactPage = new ContactPage();
     protected Header header = new Header();
     protected TestData testData;
 
     @BeforeAll
-    static void beforeAll() {
-        baseUrl = "https://automationexercise.com";
-        pageLoadStrategy = "eager";
-        browser = System.getProperty("browser", "chrome");
-        browserVersion = System.getProperty("browserVersion", "128.0");
-        browserSize = System.getProperty("browserSize", "1920x1080");
-        remote = System.getProperty("remote",
+    static void configureBrowser() {
+        Configuration.baseUrl = "https://automationexercise.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "128.0");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.remote = System.getProperty("remote",
                 "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String,
-                Object>of("enableVNC", true, "enableVideo", true));
-        browserCapabilities = capabilities;
-        open("/");
-        try {
-            $(".fc-cta-consent").should(appear, Duration.ofSeconds(10)).click();
-        } catch (Throwable ignored) {
-        }
-        $(".logo").shouldBe(visible);
+        capabilities.setCapability("selenoid:options", Map.of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
 
     @BeforeEach
     public void setUp() {
         testData = new TestData();
+        open("/");
+        try {
+            $(".fc-cta-consent").should(appear, Duration.ofSeconds(10)).click();
+        } catch (Throwable ignored) {}
+        $(".logo").shouldBe(visible);
     }
 
     @AfterEach
@@ -61,7 +62,7 @@ public class TestBase {
     }
 
     @AfterAll
-    static void videoReport() {
+    static void closeBrowser() {
         Attach.addVideo();
         closeWebDriver();
     }
