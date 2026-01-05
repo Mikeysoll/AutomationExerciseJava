6e61186c19f44b579aef227999833e6afe998c80
Mikhail Solovyov <solovev02@yandex.ru>
2026-01-05 17:19:27 +0300

    minor

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -32,8 +32,9 @@ public class TestBase {
         browser = System.getProperty("browser", "chrome");
         browserVersion = System.getProperty("browserVersion", "128.0");
         browserSize = System.getProperty("browserSize", "1920x1080");
-        holdBrowserOpen = true;
-
+        //holdBrowserOpen = true;
+        remote = System.getProperty("remote",
+                "https://user1:1234@selenoid.autotests.cloud/wd/hub");
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
